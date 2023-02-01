c214bbf8e90b73d4691233e0d42f98ccb64e2afd
Arto Antikainen <ArtoA@users.noreply.github.com>
2023-02-01 08:33:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,11 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section
 streamlit.header("Fruityvice Fruit Advice!")
-
-import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+import requests
 #
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #
@@ -39,7 +40,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# ...
+fruit_choice2 = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice2)
+fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice2)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
